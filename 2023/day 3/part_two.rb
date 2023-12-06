40446f951fa6589834a07b7b50d60ebbf4863570
Nery Campusano <nery@megsoftconsulting.com>
2023-12-05 19:58:33 -0500

    day 3

--- 2023/day 3/part_two.rb
+++ 2023/day 3/part_two.rb
@@ -1,6 +1,6 @@
 input = File.read("input.txt").split("\n")
-special_chars = input.flat_map { |value| value.scan(/\D/).reject {|v| v == "."} || v.empty? }.uniq
-results = []
+special_chars = input.flat_map { |value| value.scan(/\D/).select { |v| v == "*" } }.uniq
+results =  Hash.new { |h, k| h[k] = [] }
 
 input.each_with_index do |value, i|
   j = 0
@@ -10,10 +10,12 @@ input.each_with_index do |value, i|
     if number.match?(/\d/)
       rows = ([0, i - 1].max..[input.length - 1, i + 1].min).to_a
       cols = ([0, j - 1].max..[input[i].length - 1, j + number.length].min).to_a
-      rows.product(cols).each { |row, col| results << number.to_i if special_chars.include?(input[row][col]) }
+      rows.product(cols).each do |row, col|
+        results["#{row}#{col}"] << number.to_i if special_chars.include?(input[row][col])
+      end
     end
     j += number.length
   end
 end
 
-puts results.sum
+puts results.select {|k, v| v.length == 2 }.map {|(key,value)| value.reduce(:*)}.sum
