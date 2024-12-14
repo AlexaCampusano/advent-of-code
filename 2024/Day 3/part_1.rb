d8e32bcd50bd2064acf876fe029f09d2e1876ddf
Nery Campusano <nery@megsoftconsulting.com>
2024-12-14 14:44:13 -0500

    part 1 no regex ish

--- 2024/Day 3/part_1.rb
+++ 2024/Day 3/part_1.rb
@@ -4,6 +4,27 @@ def mul(string)
   string.scan(/mul\((\d+),(\d+)\)/).map { |x, y| x.to_i * y.to_i }.sum
 end
 
-total = input.map { |line| mul(line) }.sum
+def process_multiplications(input)
+  total = 0
+  input.each do |line|
+    index = 0
+    while index <= line.size 
+      mul_start_idx = line.index("mul(", index)
+  
+      unless mul_start_idx.nil?
+        mul_end_idx = line.index(")", mul_start_idx)
+        content = line[mul_start_idx..mul_end_idx]
+        total +=  mul(content)
+        index = mul_end_idx
+      end
+  
+      index += 1
+    end
+  end
 
-p total
+  total
+end
+
+result = process_multiplications(input)
+
+p result
